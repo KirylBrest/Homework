d85795943e01a41e7e582628b7564ccd5c515fad
Kiryl_Bakhur <bahur201554@gmail.com>
2022-08-18 14:44:33 +0300

    Добавлено дом.задание

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// Console.WriteLine("Введите число X и Y: ");
+// Console.Write("X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (  x<y )
+// {
+//     Console.WriteLine ( y );
+// }
+// else
+// {
+//      Console.WriteLine ( x );
+
+// }
+
+// Console.WriteLine("Введите три разных числа: ");
+// Console.Write("1: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("2: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.Write("3: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int max = x;
+// if ( max<y)
+// {
+//     max = y;
+// }
+// if ( max<i)
+// {
+//     max = i;
+// }
+// if ( max<x)
+// {
+//     max = x;
+// }
+// Console.WriteLine ($" Наибольшее число: {max} ");
+
+// Console.WriteLine("Введите число: ");
+// Console.Write("Число: ");
+// double x = Convert.ToInt32(Console.ReadLine());
+// if ( x % 2 == 0)
+// {
+//     Console.WriteLine("четное");
+// }
+// else
+// {
+//     Console.WriteLine("нечетное");
+// }
+
+// Console.WriteLine("Введите число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// for (int y = 1; y < x; y++)
+//     if (y % 2 == 0)
+//         Console.WriteLine(y);
